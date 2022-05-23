96cb03e7ec577086e941417d84527e5a7a5b8f30
punyprogrammer <johnmayer190999@gmail.com>
2022-05-23 20:24:16 +0530

    added-redux-and-homepage-ui

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-MONGO_URI:'mongodb+srv://amardeepganguly:amardeep123@cluster0.keg5r.mongodb.net/bookit?retryWrites=true&w=majority'
-  }
-}
+  env: {
+    MONGO_URI:
+      "mongodb+srv://amardeepganguly:amardeep123@cluster0.keg5r.mongodb.net/bookit?retryWrites=true&w=majority",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ redux/reducers/reducers.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import { allRoomsReducer } from "./roomReducers";
+const reducer = combineReducers({
+  allRooms: allRoomsReducer,
+});
+export default reducer;
