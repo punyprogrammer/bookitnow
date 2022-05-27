0eedf3f2567889600696b90018a7042762f1510d
punyprogrammer <johnmayer190999@gmail.com>
2022-05-27 15:00:21 +0530

    added-login-authentication

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+import Login from "../components/auth/Login";
+
+const LoginPage = () => {
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  );
+};
+
+export default LoginPage;
